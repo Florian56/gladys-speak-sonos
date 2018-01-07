b65d3e1bcdf4d6d89229ddd0130d4eac65b67fce
Florian56 <net.lecraver@free.fr>
2018-01-07 20:58:00 +0100

    Ajout saySongName.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ module.exports = function(sails) {
     var say = require('./lib/say.js');
     var install = require('./lib/install.js');
     var notify = require('./lib/notify.js');
+    var saySongName = require('./lib/saySongName.js');
 
     return {
         say: say,
         install: install,
-        notify: notify
+        notify: notify,
+        saySongName: saySongName
     };
 };
