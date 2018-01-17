1662c104425f459935a49b4e7213476c3f25c15c
Florian56 <net.lecraver@free.fr>
2018-01-17 19:39:05 +0100

    Add uninstall function.

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ module.exports = function(sails) {
     
     var say = require('./lib/say.js');
     var install = require('./lib/install.js');
+    var uninstall = require('./lib/uninstall.js');
     var notify = require('./lib/notify.js');
     var saySongName = require('./lib/saySongName.js');
 
     return {
         say: say,
         install: install,
+        uninstall: uninstall,
         notify: notify,
         saySongName: saySongName
     };
